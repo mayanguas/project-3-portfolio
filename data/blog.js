d75d557307bc394de198fbcf536b7309686bdf2c
mayanguas <alejandro.yanguas@yahoo.es>
2023-04-06 22:40:24 +0200

    feat: create blog.js

---
+++ data/blog.js
@@ -0,0 +1,98 @@
+export const BLOG = [
+  {
+    id: 1,
+    title: 'Crear paginador con scroll infinito',
+    date: '08/11/2022',
+    description:
+      'A continuación te ofrezco un tutorial para aprender a hacer un paginador infinito que sea moderno, eficiente y flexible ante cualquier navegador o dispositivo.Te encuentras en una continuación del artículo...',
+    duration: '6 minutos',
+  },
+  {
+    id: 2,
+    title: 'Cómo formatear una etiqueta HTML',
+    date: '15/01/2023',
+    description:
+      'Para mejorar la lectura del código, voy a enseñarte unas sencillas reglas para mejorar la legibilidad de tus documentos HTML. No son difíciles de aplicar y mejorarán enormemente el mantenimiento...',
+    duration: '2 minutos',
+  },
+  {
+    id: 3,
+    title: '¿Cuál es el mejor navegador web?',
+    date: '27/01/2023',
+    description:
+      'Chrome continúa aumentando su uso, siendo actualmente el navegador más popular (casi por defecto) de la mayoría, debido por a su marketing, integración en ChromeBooks y Androids. Por otro lado...',
+    duration: '5 minutos',
+  },
+  {
+    id: 4,
+    title: 'Modificar y devolver JSON nuevo',
+    date: '12/02/2023',
+    description:
+      'Cuando intentamos poner en prácticas ciertas prácticas funcionales como la inmutabilidad en JavaScript se nos hace complejo por el propio diseño del lenguaje. No obstante podemos navegar por rutas alternativas...',
+    duration: '2 minutos',
+  },
+  {
+    id: 5,
+    title: 'Desventajas del texto plano',
+    date: '23/02/2023',
+    description:
+      'El amor hacia el texto plano es natural e inevitable entre los técnicos de redes, administradores de sistemas o desarrolladores de software. Es la base de cualquier configuración mínima o...',
+    duration: '3 minutos',
+  },
+  {
+    id: 6,
+    title: 'Focalizar mediante desenfoque',
+    date: '01/03/2023',
+    description:
+      'Cuando queremos centrar la atención de un visitante en un lugar determinado, como puede ser un modal o un popup, disponemos de varias técnicas. Una de las más populares es...',
+    duration: '4 minutos',
+  },
+  {
+    id: 7,
+    title: 'JavaScript. Quitar acentos',
+    date: '07/03/2023',
+    description:
+      'Con la siguiente función en JavaScript podrás eliminar todos los acentos de un texto o string./** * Devuelve un texto sin acentos * @param {string} text - Texto con acentos....',
+    duration: '2 minutos',
+  },
+  {
+    id: 8,
+    title: 'Ejemplo de metrónomo en JavaScript',
+    date: '12/03/2023',
+    description:
+      'A continuación puedes ver un minimalista y sencillo metrónomo en JavaScript plano (Vainilla) adaptado a smartphone o escritorio. Con la ayuda de la etiqueta &amp;lt;audio&amp;gt; verás que es posible reproducir...',
+    duration: '8 minutos',
+  },
+  {
+    id: 9,
+    title: 'Cómo fijar temporalmente un elemento',
+    date: '18/03/2023',
+    description:
+      'A continuación vamos a aprender a fijar un elemento en el scroll de forma temporal mientras se hace scroll hasta que encuentre otro elemento que choque.Para lograrlo haremos uso de...',
+    duration: '5 minutos',
+  },
+  {
+    id: 10,
+    title: 'Menú responsive con botón hamburguesa',
+    date: '22/03/2023',
+    description:
+      'En el siguiente tutorial, o código de ejemplo, podéis trabajar con un completo menú Responsive Design que se adapta tanto a escritorio como smartphone. Trabaja de diferente forma dependiendo del...',
+    duration: '4 minutos',
+  },
+  {
+    id: 11,
+    title: 'Cambiar anchura de un elemento HTML con el ratón',
+    date: '28/03/2023',
+    description:
+      'Para modificar la anchura de cualquier elemento en con cursor del ratón, o por medio de un dedo y una pantalla touch, disponemos de una opción en CSS y otra...',
+    duration: '3 minutos',
+  },
+  {
+    id: 12,
+    title: 'Actualizar de Debian 10 a Debian 11(Bullseye)',
+    date: '03/04/2023',
+    description:
+      'Con la nueva versión estable de Debian toca actualizar de Debian 10 (buster) a Debian 11 (bullseye) para disponer de las últimas versiones del software o actualizaciones de seguridad. Por...',
+    duration: '6 minutos',
+  },
+];
